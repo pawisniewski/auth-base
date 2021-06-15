c84c522817fbab7e03126cbdb242d332680b4734
Pawel Wisniewski <pawel_wisniewski@outlook.com>
2021-06-15 13:31:48 +0200

    Clear code

--- server.js
+++ server.js
@@ -3,37 +3,23 @@ const cors = require('cors');
 const path = require('path');
 const hbs = require('express-handlebars');
 const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const session = require('express-session');
+const passportConfig = require('./config/passport');
 
 const app = express();
 
-// configure passport provider options
-passport.use(new GoogleStrategy({
-  clientID: '641655260616-op2k5eda2pb4qppc0k9msf2nok3na5s5.apps.googleusercontent.com',
-  clientSecret: 'sNZRF45Y_Fii1Dmt7ZzTBqCO',
-  callbackURL: 'http://localhost:8000/auth/google/callback'
-}, (accessToken, refreshToken, profile, done) => {
-done(null, profile);
-}));
-
-// serialize user when saving to session
-passport.serializeUser((user, serialize) => {
-  serialize(null, user);
-});
-
-// deserialize user when reading from session
-passport.deserializeUser((obj, deserialize) => {
-  deserialize(null, obj);
-});
-
+// set handlebars as view engine
 app.engine('hbs', hbs({ extname: 'hbs', layoutsDir: './layouts', defaultLayout: 'main' }));
 app.set('view engine', '.hbs');
 
+// init session mechanism
 app.use(session({ secret: 'anything' }));
+
+// init passport
 app.use(passport.initialize());
 app.use(passport.session());
 
+// standard middleware
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -43,23 +29,11 @@ app.get('/', (req, res) => {
   res.render('index');
 });
 
-app.get('/auth/google',
-  passport.authenticate('google', { scope: ['email', 'profile'] })
+app.use('/auth', require('./routes/auth.routes')
 );
 
-app.get('/auth/google/callback', passport.authenticate('google', { failureRedirect: '/user/no-permission' }),
-  (req, res) => {
-    res.redirect('/user/logged');
-  }
+app.use('/user', require('./routes/user.routes')
 );
-
-app.get('/user/logged', (req, res) => {
-  res.render('logged');
-});
-
-app.get('/user/no-permission', (req, res) => {
-  res.render('noPermission');
-});
 
 app.use('/', (req, res) => {
   res.status(404).render('notFound');
